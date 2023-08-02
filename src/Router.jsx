05aecadf14331635220a4a7db6700bbf208017b5
Augustine Ugwu <105177548+augustine-ugwu@users.noreply.github.com>
2023-08-02 15:40:25 +0100

    Hot fix 3

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import Home from "./views/Home";
-import Contact from "./views/Contact";
-import NotFound from "./views/NotFound";
 import GuestLayout from "./components/GuestLayout";
 import DefaultLayout from "./components/DefaultLayout";
 import { createBrowserRouter } from "react-router-dom";
-import Hire from "./views/Hire";
-import Services from "./views/Services";
 import About from "./views/About";
+import Home from "./views/Home";
+import Contact from "./views/Contact";
+import NotFound from "./views/NotFound";
+import Services from "./views/Services";
+import Hire from "./views/Hire";
 
 const Router = createBrowserRouter([
   {
