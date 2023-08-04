f99be233ca15ea8bda58d5ca7159897b57adc0cd
Augustine Ugwu <105177548+augustine-ugwu@users.noreply.github.com>
2023-08-04 12:44:35 +0100

    Refreshing views Dir

--- src/Views/NotFound.jsx
+++ src/Views/NotFound.jsx
@@ -1,16 +1,7 @@
 import React from "react";
-import { Flex, Text } from "@chakra-ui/react";
 
 const NotFound = () => {
-  return (
-    <>
-      <Flex height={"100vh"} alignItems={"center"} justifyContent={"center"}>
-        <Text fontWeight={700} fontSize={30}>
-          404 - Page Not Found
-        </Text>
-      </Flex>
-    </>
-  );
+  return <div>NotFound</div>;
 };
 
 export default NotFound;
